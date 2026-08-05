9ce856da7f4b185ac14e639e6228c2a3c6ee794c
Hank <13269982528@163.com>
2026-08-06 00:13:10 +0800

    configure github pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  turbopack: {
-    root: __dirname
-  }
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/METC-website",
+  assetPrefix: "/METC-website/",
 };
 
 export default nextConfig;
